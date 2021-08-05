41ad7c91f6488669a58d7c7c77668ed7b1d8bbd2
Andy Santisteban <64505022+AndySantisteban@users.noreply.github.com>
2021-08-04 20:07:05 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import 'bootswatch/dist/lux/bootstrap.min.css'
 import 'bootswatch/dist/lux/bootstrap.css'
+import 'bootswatch/dist/lux/_bootswatch.scss'
+import 'bootswatch/dist/lux/_variables.scss'
 function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
   }
